28a678539c2937d78a137ba025ae0ffa5099dead
bison <bisonmontana@gmail.com>
2017-05-28 02:14:31 +0200

    added n-meta and that dumb old application-vnd header. why not have them all in one place

---
+++ app/src/main/java/dk/nodes/kstack/ClientAppInfo.kt
@@ -0,0 +1,23 @@
+package dk.nodes.kstack
+
+import android.content.Context
+import android.R.attr.versionName
+import android.content.pm.PackageInfo
+
+
+
+/**
+ * Created by bison on 28-05-2017.
+ */
+class ClientAppInfo(context : Context) {
+    val packageName : String = context.packageName
+    val versionName : String
+    val versionCode : Int
+    var environment : String = "development"
+
+    init {
+        val pInfo = context.packageManager.getPackageInfo(packageName, 0)
+        versionName = pInfo.versionName
+        versionCode = pInfo.versionCode
+    }
+}
--- app/src/main/java/dk/nodes/kstack/providers/NStackInterceptor.kt
+++ app/src/main/java/dk/nodes/kstack/providers/NStackInterceptor.kt
@@ -1,12 +1,12 @@
 package dk.nodes.kstack.providers
 
+import android.os.Build
 import dk.nodes.kstack.KStack
 
 /**
  * Created by joso on 08/10/15.
  */
 class NStackInterceptor : okhttp3.Interceptor {
-
     @Throws(java.io.IOException::class)
     override fun intercept(chain: okhttp3.Interceptor.Chain): okhttp3.Response {
         val originalRequest = chain.request()
@@ -16,6 +16,8 @@ class NStackInterceptor : okhttp3.Interceptor {
                 //.header("Accept-Language", NStack.getStack().getSelectedLanguageHeader())
                 .header("X-Application-Id", KStack.appId)
                 .header("X-Rest-Api-Key", KStack.appKey)
+                .header("Accept", "application/vnd.nodes.v1+json")
+                .header("N-Meta", "android;${KStack.clientAppInfo.environment};${KStack.clientAppInfo.versionName} (${KStack.clientAppInfo.versionCode});${Build.VERSION.RELEASE};${Build.MODEL}")
                 .build()
 
         return chain.proceed(newRequest)
