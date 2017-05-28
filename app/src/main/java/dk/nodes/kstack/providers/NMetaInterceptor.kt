36ad1f98d2ffd20b075642a927fb32c3e61c11ae
bison <bisonmontana@gmail.com>
2017-05-28 02:43:26 +0200

    seperated ceptors

--- app/src/main/java/dk/nodes/kstack/providers/NMetaInterceptor.kt
+++ app/src/main/java/dk/nodes/kstack/providers/NMetaInterceptor.kt
@@ -6,16 +6,14 @@ import dk.nodes.kstack.KStack
 /**
  * Created by joso on 08/10/15.
  */
-class NStackInterceptor : okhttp3.Interceptor {
+class NMetaInterceptor(val environment : String = "development") : okhttp3.Interceptor {
     @Throws(java.io.IOException::class)
     override fun intercept(chain: okhttp3.Interceptor.Chain): okhttp3.Response {
         val originalRequest = chain.request()
 
         val newRequest = originalRequest.newBuilder()
-                //Commented this out because it was causing issues with the cached languageHeader
-                //.header("Accept-Language", NStack.getStack().getSelectedLanguageHeader())
-                .header("X-Application-Id", KStack.appId)
-                .header("X-Rest-Api-Key", KStack.appKey)
+                .header("Accept", "application/vnd.nodes.v1+json")
+                .header("N-Meta", "android;${environment};${KStack.clientAppInfo.versionName} (${KStack.clientAppInfo.versionCode});${Build.VERSION.RELEASE};${Build.MODEL}")
                 .build()
 
         return chain.proceed(newRequest)
