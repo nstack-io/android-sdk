fdadc2ca8955621e94e866d09f2b3f76b5f94dc7
Stinus Petersen <stpe@nodes.dk1>
2018-09-17 15:31:48 +0200

    Made a workaround for some samsung phones calling getSystemService with a null argument

--- app/src/main/java/dk/nodes/nstack/kotlin/inflater/NStackBaseContext.kt
+++ app/src/main/java/dk/nodes/nstack/kotlin/inflater/NStackBaseContext.kt
@@ -4,14 +4,23 @@ import android.content.Context
 import android.content.ContextWrapper
 import android.view.LayoutInflater
 
-class NStackBaseContext(context: Context) : ContextWrapper(context) {
-    override fun getSystemService(name: String?): Any {
 
+
+
+class NStackBaseContext(context: Context) : ContextWrapper(context) {
+
+    /**
+     * signature from Context.java:
+     * public abstract @Nullable Object getSystemService(@ServiceName @NonNull String name);
+     */
+    override fun getSystemService(name: String?): Any? {
+        // apparently sometimes this can be called with null on certain samsung phones
+        if(name == null)
+            return null
         if (Context.LAYOUT_INFLATER_SERVICE.equals(name, true)) {
             val layoutInflater = LayoutInflater.from(baseContext)
             return NStackLayoutInflater(layoutInflater, baseContext, false)
         }
-
         return super.getSystemService(name)
     }
 }
