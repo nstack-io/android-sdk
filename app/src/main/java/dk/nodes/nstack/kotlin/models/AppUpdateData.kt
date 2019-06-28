49f79e6a95dd947db770de12b4ca556221946617
Johnny Sørensen <post@johsoe.dk>
2019-06-28 17:03:37 +0200

    added support for messages

--- app/src/main/java/dk/nodes/nstack/kotlin/models/AppUpdateData.kt
+++ app/src/main/java/dk/nodes/nstack/kotlin/models/AppUpdateData.kt
@@ -6,20 +6,27 @@ import org.json.JSONObject
 import java.util.*
 
 data class AppUpdateData(
-    val count: Int = 0,
-    val update: AppUpdate = AppUpdate(),
-    val localize: List<LocalizeIndex> = listOf(),
-    val platform: String = "",
-    val createdAt: Date = Date(),
-    val lastUpdated: Date = Date()
+        val count: Int = 0,
+        var update: AppUpdate = AppUpdate(),
+        val localize: List<LocalizeIndex> = listOf(),
+        val platform: String = "",
+        val createdAt: Date = Date(),
+        val lastUpdated: Date = Date(),
+        var message: Message? = null
 ) {
 
     constructor(jsonObject: JSONObject) : this(
         count = jsonObject.optInt("count"),
-        update = AppUpdate(jsonObject.getJSONObject("update")),
         localize = jsonObject.getJSONArray("localize").localizeIndices,
         platform = jsonObject.getString("platform"),
         createdAt = jsonObject.getString("created_at").iso8601Date,
         lastUpdated = jsonObject.getString("last_updated").iso8601Date
-    )
+    ) {
+        if(jsonObject.has("update")) {
+            update = AppUpdate(jsonObject.getJSONObject("update"))
+        }
+        if(jsonObject.has("message")) {
+            message = Message(jsonObject.getJSONObject("message"))
+        }
+    }
 }
