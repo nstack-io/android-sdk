406437334c9531819ab40dcd870d8dc277a99aa2
Alexander Bondarenco <sasha.199568@gmail.com>
2019-07-31 11:29:38 +0300

    Test app open

--- app/src/main/java/dk/nodes/nstack/kotlin/util/ContextWrapper.kt
+++ app/src/main/java/dk/nodes/nstack/kotlin/util/ContextWrapper.kt
@@ -1,27 +1,28 @@
 package dk.nodes.nstack.kotlin.util
 
 import android.content.Context
+import android.os.Handler
 
-/**
- * Use context indirectly for testing
- */
+
 internal class ContextWrapper(
     private val context: Context
 ) {
 
-    /**
-     * List of files in assets
-     */
+    private var handler: Handler = Handler()
+
     val assets: List<String>
         get() {
             return context.resources.assets.list("")?.asList() ?: listOf()
         }
 
-    /**
-     * Opens and reads an asset into string
-     */
     fun readAsset(path: String): String {
         val inputStream = context.resources.assets.open(path)
         return inputStream.bufferedReader().use { it.readText() }
+    }
+
+    fun runUiAction(action: () -> Unit) {
+        handler.post {
+            action()
+        }
     }
 }
