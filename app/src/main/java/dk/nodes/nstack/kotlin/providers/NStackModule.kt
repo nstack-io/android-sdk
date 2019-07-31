2620ce87617343ab75f4de8f79b5e36a85824153
lucassales <saless182@gmail.com>
2019-07-31 09:59:22 +0200

    Update NStackModule.kt

--- app/src/main/java/dk/nodes/nstack/kotlin/providers/NStackModule.kt
+++ app/src/main/java/dk/nodes/nstack/kotlin/providers/NStackModule.kt
@@ -5,7 +5,7 @@ import dk.nodes.nstack.kotlin.managers.AppOpenSettingsManager
 import dk.nodes.nstack.kotlin.managers.PrefManager
 import dk.nodes.nstack.kotlin.util.Preferences
 import dk.nodes.nstack.kotlin.util.PreferencesImpl
-
+import kotlin.reflect.KClass
 
 /**
  * Provides dependencies for NStack
@@ -16,17 +16,32 @@ class NStackModule(private val context: Context) {
      * Creates new AppOpenSettingsManager
      */
     fun provideAppOpenSettingsManager(): AppOpenSettingsManager {
-        return AppOpenSettingsManager(context, providePreferences())
+        return getLazyDependency(AppOpenSettingsManager::class) {
+            AppOpenSettingsManager(
+                context,
+                providePreferences()
+            )
+        }
     }
 
     /**
      * Creates new PrefManager
      */
     fun providePrefManager(): PrefManager {
-        return PrefManager(providePreferences())
+        return getLazyDependency(PrefManager::class) { PrefManager(providePreferences()) }
     }
 
     private fun providePreferences(): Preferences {
-        return PreferencesImpl(context)
+        return getLazyDependency(PreferencesImpl::class) { PreferencesImpl(context) }
     }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun <T : Any> getLazyDependency(klass: KClass<T>, block: () -> T): T {
+        if (!dependenciesMap.containsKey(klass)) {
+            dependenciesMap[klass] = block()
+        }
+        return dependenciesMap[klass] as T
+    }
+
+    private val dependenciesMap = mutableMapOf<KClass<*>, Any>()
 }
