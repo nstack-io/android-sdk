2f4e63d4e582c84f4919f491f42aba3792c14e1f
lucassales <saless182@gmail.com>
2019-07-31 11:15:58 +0200

    Remove build type and updating target/compile api

--- app/src/main/java/dk/nodes/nstack/kotlin/inflater/NStackBaseContext.kt
+++ app/src/main/java/dk/nodes/nstack/kotlin/inflater/NStackBaseContext.kt
@@ -11,11 +11,8 @@ class NStackBaseContext(context: Context) : ContextWrapper(context) {
      * signature from Context.java:
      * public abstract @Nullable Object getSystemService(@ServiceName @NonNull String name);
      */
-    override fun getSystemService(name: String?): Any? {
+    override fun getSystemService(name: String): Any? {
         // apparently sometimes this can be called with null on certain samsung phones
-        if (name == null) {
-            return null
-        }
 
         if (Context.LAYOUT_INFLATER_SERVICE.equals(name, true)) {
             return NStackLayoutInflater(LayoutInflater.from(baseContext), baseContext, false)
