d172a545aab77b95db4294224e37836977ed34fb
lucassales <saless182@gmail.com>
2019-07-31 09:41:51 +0200

    Live editing on xml

--- demo/src/main/java/dk/nodes/nstack/demo/MainActivity.kt
+++ demo/src/main/java/dk/nodes/nstack/demo/MainActivity.kt
@@ -1,29 +1,25 @@
 package dk.nodes.nstack.demo
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dk.nodes.nstack.kotlin.NStack
-import dk.nodes.nstack.kotlin.inflater.NStackRootLayout
-import dk.nodes.nstack.kotlin.util.extensions.plusAssign
+import dk.nodes.nstack.kotlin.inflater.NStackBaseContext
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val nStackRootLayout = NStackRootLayout(this)
-        layoutInflater.inflate(R.layout.activity_main, nStackRootLayout)
-        setContentView(nStackRootLayout)
-
+        setContentView(R.layout.activity_main)
         NStack.setTranslation(
             toolbar,
             nstackKey = getString(R.string.nstack_test_title),
             title = "test_title"
         )
+    }
 
-        messageTextView += getString(R.string.nstack_test_message)
-        yesButton += getString(R.string.nstack_default_yes)
-        noButton += getString(R.string.nstack_default_no)
+    override fun attachBaseContext(newBase: Context) {
+        super.attachBaseContext(NStackBaseContext(newBase))
     }
 }
