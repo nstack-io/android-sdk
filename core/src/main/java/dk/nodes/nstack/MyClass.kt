d5883cddb185a8051e839956a8b842336447d821
lucassales <saless182@gmail.com>
2019-08-02 11:03:57 +0200

    Modularizing

---
+++ core/src/main/java/dk/nodes/nstack/MyClass.kt
@@ -0,0 +1,3 @@
+package dk.nodes.nstack
+
+class MyClass
--- demo/src/main/java/dk/nodes/nstack/demo/MainActivity.kt
+++ demo/src/main/java/dk/nodes/nstack/demo/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dk.nodes.nstack.kotlin.NStack
 import dk.nodes.nstack.kotlin.inflater.NStackBaseContext
+import dk.nodes.nstack.kotlin.util.extensions.plusAssign
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        messageTextView += getString(R.string.nstack_test_title)
     }
 
     override fun attachBaseContext(newBase: Context) {
