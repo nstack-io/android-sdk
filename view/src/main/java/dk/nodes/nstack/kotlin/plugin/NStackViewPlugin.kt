98ff6948a0feb588a157357893b50dc1a81ab78c
lucassales <saless182@gmail.com>
2019-08-06 14:57:00 +0200

    Making core android independent

--- view/src/main/java/dk/nodes/nstack/kotlin/plugin/NStackViewPlugin.kt
+++ view/src/main/java/dk/nodes/nstack/kotlin/plugin/NStackViewPlugin.kt
@@ -2,6 +2,7 @@ package dk.nodes.nstack.kotlin.plugin
 
 import android.view.View
 import dk.nodes.nstack.kotlin.models.TranslationData
+import dk.nodes.nstack.kotlin.plugin.NStackPlugin
 import java.lang.ref.WeakReference
 
 interface NStackViewPlugin: NStackPlugin {
