df99f019db600692778c401bb0a2c3f944a06c8c
Johnny Sørensen <post@johsoe.dk>
2019-08-07 15:20:57 +0200

    fixed Message field change from newest API deploy

--- app/src/main/java/dk/nodes/nstack/kotlin/models/Message.kt
+++ app/src/main/java/dk/nodes/nstack/kotlin/models/Message.kt
@@ -4,7 +4,7 @@ import org.json.JSONObject
 
 data class Message(
         val id: Int,
-        val projectId: Int,
+        val applicationId: Int,
         val platform: String,
         val showSetting: String,
         val viewCount: Int,
@@ -14,7 +14,7 @@ data class Message(
 
     constructor(jsonObject: JSONObject): this(
             id = jsonObject.getInt("id"),
-            projectId = jsonObject.getInt("project_id"),
+            applicationId = jsonObject.getInt("application_id"),
             platform = jsonObject.getString("platform"),
             showSetting = jsonObject.getString("show_setting"),
             viewCount = jsonObject.getInt("view_count"),
