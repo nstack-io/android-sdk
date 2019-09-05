1f95a7ede3807b758f8ffd84a42ae66146c8b3b5
maru <maru@nodesagency.com>
2019-09-05 16:09:26 +0200

    Cleanup

--- nstack-kotlin/src/main/java/dk/nodes/nstack/kotlin/util/extensions/ViewExtensions.kt
+++ nstack-kotlin/src/main/java/dk/nodes/nstack/kotlin/util/extensions/ViewExtensions.kt
@@ -1,17 +1,6 @@
 package dk.nodes.nstack.kotlin.util.extensions
 
 import android.view.View
-import android.widget.ProgressBar
-
-fun ProgressBar.show() {
-    animation?.cancel()
-    animate().alpha(1f).setDuration(200L).setStartDelay(200L).withStartAction { visibility = View.VISIBLE }.start()
-}
-
-fun ProgressBar.hide() {
-    animation?.cancel()
-    animate().alpha(0f).setDuration(200L).setStartDelay(0L).withEndAction { visibility = View.GONE }.start()
-}
 
 fun View.show() {
     animation?.cancel()
