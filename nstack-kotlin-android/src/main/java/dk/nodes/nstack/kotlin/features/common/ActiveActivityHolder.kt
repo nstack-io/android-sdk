5fdcbe6f80f26ea9a4551d106cdde13b90943998
Matko Smoljan <mats@nodesagency.com>
2019-10-14 15:15:09 +0200

    Create holder class for active activities

---
+++ nstack-kotlin-android/src/main/java/dk/nodes/nstack/kotlin/features/common/ActiveActivityHolder.kt
@@ -0,0 +1,51 @@
+package dk.nodes.nstack.kotlin.features.common
+
+import android.app.Activity
+import android.app.Application
+import android.os.Bundle
+import java.lang.ref.WeakReference
+
+class ActiveActivityHolder : Application.ActivityLifecycleCallbacks {
+
+    var isInForeground: Boolean = false
+        private set
+
+    var foregroundActivity: Activity? = null
+        get() = foregroundActivityReference.get()
+        private set // Not used and shouldn't be! The weak reference is used for this!
+
+    var foregroundActivityReference: WeakReference<Activity?> = WeakReference(null)
+        private set
+
+    override fun onActivityPaused(activity: Activity) {
+        if (foregroundActivity != null && foregroundActivity === activity) {
+            isInForeground = false
+        }
+    }
+
+    override fun onActivityStarted(activity: Activity) {
+        foregroundActivityReference = WeakReference(activity)
+    }
+
+    override fun onActivityStopped(activity: Activity) {
+        if (foregroundActivity != null && foregroundActivity === activity) {
+            foregroundActivityReference = WeakReference(null)
+        }
+    }
+
+    override fun onActivityResumed(activity: Activity) {
+        isInForeground = true
+    }
+
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+        // Do nothing, it's not shown yet
+    }
+
+    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+        // Do nothing, we only care about holding the foreground activities and have no interest in the instance state
+    }
+
+    override fun onActivityDestroyed(activity: Activity) {
+        // Do nothing, should be already removed from the foregroundActivity variable at this point
+    }
+}
