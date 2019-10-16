f72f007a1e39dd25802cdc2a4f3a5cf1f96289df
maru <maru@nodesagency.com>
2019-10-16 10:29:41 +0200

    Log AppOpenResult

--- demo/src/main/java/dk/nodes/nstack/demo/Application.kt
+++ demo/src/main/java/dk/nodes/nstack/demo/Application.kt
@@ -3,6 +3,11 @@ package dk.nodes.nstack.demo
 import android.app.Application
 import android.util.Log
 import dk.nodes.nstack.kotlin.NStack
+import dk.nodes.nstack.kotlin.models.AppOpenResult
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class Application : Application() {
 
@@ -15,18 +20,21 @@ class Application : Application() {
         if (BuildConfig.DEBUG) {
             NStack.enableLiveEdit(this)
         }
-        NStack.appOpen {
-            Log.d("AppOpen", it.toString())
 
-            val terms = NStack.Terms.getAppOpenTerms()
-
-            Log.d("AppOpenTerms", terms.toString())
+        GlobalScope.launch {
+            withContext(Dispatchers.IO) {
+                when (val result : AppOpenResult = NStack.appOpen()) {
+                    is AppOpenResult.Success -> {
+                        Log.d("AppOpenResult", result.toString())
+                    }
+                    is AppOpenResult.Failure -> {
+                        Log.d("AppOpenResult", "Failure")
+                    }
+                    is AppOpenResult.NoInternet -> {
+                        Log.d("AppOpenResult", "NoInternet")
+                    }
+                }
+            }
         }
-
-
-        //NStack.Terms.getLatestTerms(1)
-        // NStack.Terms.getTerms(1)
-
-        NStack.Feedback.appVersion
     }
 }
