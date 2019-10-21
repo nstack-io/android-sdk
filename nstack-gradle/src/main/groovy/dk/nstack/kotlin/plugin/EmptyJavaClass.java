5565c8031a11db834ee8af166eaeb73b63d3ad5a
lucassales <saless182@gmail.com>
2019-10-21 09:59:18 +0200

    Adding code style to groovy, gradle and java files

--- nstack-gradle/src/main/groovy/dk/nstack/kotlin/plugin/EmptyJavaClass.java
+++ nstack-gradle/src/main/groovy/dk/nstack/kotlin/plugin/EmptyJavaClass.java
@@ -3,12 +3,8 @@ package dk.nstack.kotlin.plugin;
 public class EmptyJavaClass {
 
     /**
-     * Workaround class file for gradle bug:
-     * https://issues.gradle.org/browse/GRADLE-3169 or https://issues.sonatype.org/browse/OSSRH-637
-     * This is a groovy project only
+     * Workaround class file for gradle bug: https://issues.gradle.org/browse/GRADLE-3169 or
+     * https://issues.sonatype.org/browse/OSSRH-637 This is a groovy project only
      */
-    public void testFunction() {
-
-    }
-
+    public void testFunction() {}
 }
