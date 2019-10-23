67aadd988a458b53ddcf5d335e29d22cddc5e6d3
maru <maru@nodesagency.com>
2019-10-23 16:44:26 +0200

    Fix demo

--- demo/src/main/java/dk/nodes/nstack/demo/home/HomeFragment.kt
+++ demo/src/main/java/dk/nodes/nstack/demo/home/HomeFragment.kt
@@ -1,6 +1,18 @@
 package dk.nodes.nstack.demo.home
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import dk.nodes.nstack.demo.R
+import dk.nodes.nstack.kotlin.NStack
+import kotlinx.android.synthetic.main.fragment_home.*
 
-class HomeFragment : Fragment(R.layout.fragment_home)
+class HomeFragment : Fragment(R.layout.fragment_home) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        yesButton.setOnClickListener {
+            NStack.Feedback.show(view.context)
+        }
+    }
+}
