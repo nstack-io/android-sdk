603439936adaf64ac1e22654b3c406e3e43c872c
maru <maru@nodesagency.com>
2019-10-25 15:47:51 +0200

    Cleanup imports

--- demo/src/main/java/dk/nodes/nstack/demo/Application.kt
+++ demo/src/main/java/dk/nodes/nstack/demo/Application.kt
@@ -1,13 +1,7 @@
 package dk.nodes.nstack.demo
 
 import android.app.Application
-import android.util.Log
 import dk.nodes.nstack.kotlin.NStack
-import dk.nodes.nstack.kotlin.models.Result
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class Application : Application() {
 
