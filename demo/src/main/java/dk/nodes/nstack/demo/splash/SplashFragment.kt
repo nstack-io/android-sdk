7c0c8501a063ea5214a0909cbaa5d21583823e2a
maru <maru@nodesagency.com>
2019-10-25 12:35:05 +0200

    Show splash screen for at least 1,5 seconds
    WIP Fix: Lifecycle around appOpen()

--- demo/src/main/java/dk/nodes/nstack/demo/splash/SplashFragment.kt
+++ demo/src/main/java/dk/nodes/nstack/demo/splash/SplashFragment.kt
@@ -2,34 +2,52 @@ package dk.nodes.nstack.demo.splash
 
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation.findNavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import dk.nodes.nstack.demo.R
 import dk.nodes.nstack.kotlin.NStack
 import dk.nodes.nstack.kotlin.models.Result
-import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-class SplashFragment : Fragment(R.layout.fragment_splash) {
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+private const val DURATION = 1500L
 
-        GlobalScope.launch {
+class SplashFragment : Fragment(R.layout.fragment_splash) {
+
+    private var appOpenJob : Job? = null
+
+    private val splashTimer = SplashTimer(DURATION) {
+        findNavController().navigate(R.id.mainFragment)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        lifecycle.addObserver(splashTimer)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        appOpenJob = GlobalScope.launch {
             withContext(Dispatchers.IO) {
                 when (val result = NStack.appOpen()) {
-                    is Result.Success ->  Log.d("AppOpenResult: Success", result.toString())
+                    is Result.Success -> Log.d("AppOpenResult: Success", result.toString())
                     is Result.Error -> Log.d("AppOpenResult: Error", result.toString())
                 }
                 withContext(Dispatchers.Main) {
-                    findNavController(view).navigate(R.id.mainFragment)
+                    splashTimer.finish()
                 }
             }
         }
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        appOpenJob?.cancel()
     }
 }
