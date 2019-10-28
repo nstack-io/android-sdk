4b954af40e6f1fd12e27a73cc8f8803d1c085754
maru <maru@nodesagency.com>
2019-10-28 12:05:54 +0100

    Use ViewModelProvider instead of deprecated ViewModelProviders

--- demo/src/main/java/dk/nodes/nstack/demo/ratereminder/RateReminderFragment.kt
+++ demo/src/main/java/dk/nodes/nstack/demo/ratereminder/RateReminderFragment.kt
@@ -5,7 +5,7 @@ import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import dk.nodes.nstack.demo.R
 import dk.nodes.nstack.demo.Translation
@@ -21,7 +21,7 @@ class RateReminderFragment : Fragment(R.layout.fragment_ratereminder) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        viewModel = ViewModelProviders.of(this)[RateReminderViewModel::class.java]
+        viewModel = ViewModelProvider(this).get(RateReminderViewModel::class.java)
         viewModel.viewState.observe(this, Observer(this::showViewState))
     }
 
