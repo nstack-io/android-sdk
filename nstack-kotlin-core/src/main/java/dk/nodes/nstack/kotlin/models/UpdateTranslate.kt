c86ffc58616579b6e921a308a5279b1d70bb7933
chnt <chnt@nodes.dk>
2020-02-04 10:40:27 +0100

    Fixed appupdate bug: Positive and negative buttons had wrong Translate names

--- nstack-kotlin-core/src/main/java/dk/nodes/nstack/kotlin/models/UpdateTranslate.kt
+++ nstack-kotlin-core/src/main/java/dk/nodes/nstack/kotlin/models/UpdateTranslate.kt
@@ -1,8 +1,9 @@
 package dk.nodes.nstack.kotlin.models
+import com.google.gson.annotations.SerializedName
 
 data class UpdateTranslate(
-    val title: String,
-    val message: String,
-    val positiveButton: String,
-    val negativeButton: String
+        val title: String,
+        val message: String,
+        @SerializedName("positiveBtn") val positiveButton: String,
+        @SerializedName("negativeBtn") val negativeButton: String
 )
