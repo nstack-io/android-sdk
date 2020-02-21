3ddd74917f6e93d6e8fd94ef8568b09030aa13a7
lucassales <saless182@gmail.com>
2020-02-21 10:23:20 +0100

    Dependency updates and AppUpdateFragment

---
+++ demo/src/main/java/dk/nodes/nstack/demo/update/AppUpdateFragment.kt
@@ -0,0 +1,15 @@
+package dk.nodes.nstack.demo.update
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import dk.nodes.nstack.demo.R
+
+class AppUpdateFragment: Fragment(R.layout.fragment_app_update) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
