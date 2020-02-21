c9f4dcc0e4cfaa48dd27a00f0a6c3533bafa237a
lucassales <saless182@gmail.com>
2020-02-21 11:44:51 +0100

    App update samples

---
+++ demo/src/main/java/dk/nodes/nstack/demo/extensions/Extensions.kt
@@ -0,0 +1,8 @@
+package dk.nodes.nstack.demo.extensions
+
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+fun Fragment.showToast(message: String) {
+    Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+}
--- demo/src/main/java/dk/nodes/nstack/demo/update/AppUpdateFragment.kt
+++ demo/src/main/java/dk/nodes/nstack/demo/update/AppUpdateFragment.kt
@@ -1,35 +1,196 @@
 package dk.nodes.nstack.demo.update
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.InstallState
 import com.google.android.play.core.install.model.AppUpdateType
 import com.google.android.play.core.install.model.UpdateAvailability
 import dk.nodes.nstack.demo.R
+import dk.nodes.nstack.demo.extensions.showToast
+import dk.nodes.nstack.kotlin.NStack
+import dk.nodes.nstack.kotlin.models.AppUpdate
+import dk.nodes.nstack.kotlin.models.AppUpdateState
+import dk.nodes.nstack.kotlin.models.Result
+import dk.nodes.nstack.kotlin.models.state
+import dk.nodes.nstack.kotlin.models.update
+import kotlinx.android.synthetic.main.fragment_app_update.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import timber.log.Timber
 
-class AppUpdateFragment: Fragment(R.layout.fragment_app_update) {
+class AppUpdateFragment : Fragment(R.layout.fragment_app_update) {
+
+    private val listener = { state: InstallState ->
+        showToast("InstallState: $state")
+    }
 
     private val appUpdateManager by lazy { AppUpdateManagerFactory.create(requireContext()) }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // Returns an intent object that you use to check for an update.
-        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
-        // Checks that the platform will allow the specified type of update.
-        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
-            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
-                // For a flexible update, use AppUpdateType.FLEXIBLE
-                && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
-            ) {
-                // Request the update.
+        nstackUpdateBtn.setOnClickListener {
+            viewLifecycleOwner.lifecycleScope.launchWhenCreated {
+                checkNStackUpdate()
             }
         }
-        appUpdateInfoTask.addOnFailureListener {
-            Timber.e(it)
+
+        playstoreUpdateBtn.setOnClickListener {
+            // Returns an intent object that you use to check for an update.
+            val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+            // Checks that the platform will allow the specified type of update.
+            appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+
+                when (appUpdateInfo.updateAvailability()) {
+                    UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS -> {
+                        showToast("Update in progress")
+                    }
+                    UpdateAvailability.UNKNOWN -> {
+                        showToast("Update availability unknown")
+                    }
+                    UpdateAvailability.UPDATE_AVAILABLE -> {
+                        if (appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
+                            showToast("App Update available and immediate")
+                            appUpdateManager.startUpdateFlowForResult(
+                                // Pass the intent that is returned by 'getAppUpdateInfo()'.
+                                appUpdateInfo,
+                                // Or 'AppUpdateType.FLEXIBLE' for flexible updates.
+                                AppUpdateType.IMMEDIATE,
+                                // The current activity making the update request.
+                                requireActivity(),
+                                // Include a request code to later monitor this update request.
+                                AppUpdateRequestCode
+                            )
+                        } else if (appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)) {
+                            showToast("App Update available and flexible")
+                        }
+                    }
+                    UpdateAvailability.UPDATE_NOT_AVAILABLE -> {
+                        showToast("App Update not available")
+                    }
+                }
+                if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE &&
+                    // For a flexible update, use AppUpdateType.FLEXIBLE
+                    appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
+                ) {
+                    appUpdateManager.startUpdateFlowForResult(
+                        // Pass the intent that is returned by 'getAppUpdateInfo()'.
+                        appUpdateInfo,
+                        // Or 'AppUpdateType.FLEXIBLE' for flexible updates.
+                        AppUpdateType.IMMEDIATE,
+                        // The current activity making the update request.
+                        requireActivity(),
+                        // Include a request code to later monitor this update request.
+                        AppUpdateRequestCode
+                    )
+                }
+            }
+            appUpdateInfoTask.addOnFailureListener {
+                Timber.e(it)
+                showToast(it.toString())
+            }
+        }
+
+        // Before starting an update, register a listener for updates.
+        appUpdateManager.registerListener(listener)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        // When status updates are no longer needed, unregister the listener.
+        appUpdateManager.unregisterListener(listener)
+    }
+
+    private suspend fun checkNStackUpdate() {
+        when (val result = withContext(Dispatchers.IO) { NStack.appOpen() }) {
+            is Result.Success -> {
+                when (result.value.data.update.state) {
+                    AppUpdateState.NONE -> { /* Nothing to do */
+                    }
+                    AppUpdateState.UPDATE -> showUpdateDialog(result.value.data.update)
+                    AppUpdateState.FORCE -> showForceDialog(result.value.data.update)
+                    AppUpdateState.CHANGELOG -> showChangelogDialog(result.value.data.update)
+                }
+            }
+            is Result.Error -> {
+                showToast(result.error.toString())
+            }
         }
     }
 
+    private fun showUpdateDialog(appUpdate: AppUpdate) {
+        AlertDialog.Builder(requireContext())
+            .setTitle(appUpdate.update?.translate?.title ?: return)
+            .setMessage(appUpdate.update?.translate?.message ?: return)
+            .setPositiveButton(appUpdate.update?.translate?.positiveButton) { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
+    }
+
+    private fun showChangelogDialog(appUpdate: AppUpdate) {
+        AlertDialog.Builder(requireContext())
+            .setTitle(appUpdate.update?.translate?.title ?: return)
+            .setMessage(appUpdate.update?.translate?.message ?: return)
+            .setNegativeButton(appUpdate.update?.translate?.negativeButton ?: return) { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
+    }
+
+    private fun showForceDialog(appUpdate: AppUpdate) {
+        val dialog = AlertDialog.Builder(requireContext())
+            .setTitle(appUpdate.update?.translate?.title ?: return)
+            .setMessage(appUpdate.update?.translate?.message ?: return)
+            .setCancelable(false)
+            .setPositiveButton(appUpdate.update?.translate?.positiveButton, null)
+            .create()
+
+        dialog.setOnShowListener {
+            val b = dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+            b.setOnClickListener {
+                startPlayStore()
+            }
+        }
+
+        dialog.show()
+    }
+
+    private fun startPlayStore() {
+        try {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("market://details?id=${requireActivity().packageName}")
+                )
+            )
+        } catch (anfe: android.content.ActivityNotFoundException) {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://play.google.com/store/apps/details?id=${requireActivity().packageName}")
+                )
+            )
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == AppUpdateRequestCode) {
+            if (resultCode != RESULT_OK) {
+                showToast("Update flow failed! Result code: $resultCode")
+                // If the update is cancelled or fails,
+                // you can request to start the update again.
+            }
+        }
+    }
+
+    companion object {
+        private const val AppUpdateRequestCode = 16816
+    }
 }
