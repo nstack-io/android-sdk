f464c502773f7bdcf8a08b0f95158975f3862bf3
lucassales <saless182@gmail.com>
2020-02-21 10:39:04 +0100

    Target JVM 8

--- demo/src/main/java/dk/nodes/nstack/demo/update/AppUpdateFragment.kt
+++ demo/src/main/java/dk/nodes/nstack/demo/update/AppUpdateFragment.kt
@@ -3,13 +3,33 @@ package dk.nodes.nstack.demo.update
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.UpdateAvailability
 import dk.nodes.nstack.demo.R
+import timber.log.Timber
 
 class AppUpdateFragment: Fragment(R.layout.fragment_app_update) {
+
+    private val appUpdateManager by lazy { AppUpdateManagerFactory.create(requireContext()) }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        // Returns an intent object that you use to check for an update.
+        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+        // Checks that the platform will allow the specified type of update.
+        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                // For a flexible update, use AppUpdateType.FLEXIBLE
+                && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
+            ) {
+                // Request the update.
+            }
+        }
+        appUpdateInfoTask.addOnFailureListener {
+            Timber.e(it)
+        }
     }
 
 }
