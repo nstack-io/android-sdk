22859403539acade8cf34cbbcf2f5283389f9a8e
halulkin <halulkin@gmail.com>
2021-06-22 15:57:40 +0200

    Tested PostMessageSeen

--- nstack-kotlin-android/src/test/java/dk/nodes/nstack/kotlin/managers/NetworkManagerTestFake.kt
+++ nstack-kotlin-android/src/test/java/dk/nodes/nstack/kotlin/managers/NetworkManagerTestFake.kt
@@ -3,6 +3,7 @@ package dk.nodes.nstack.kotlin.managers
 import dk.nodes.nstack.kotlin.managers.Helper.setResponse
 import dk.nodes.nstack.kotlin.model.AppOpenSettingsMockFactory
 import dk.nodes.nstack.kotlin.models.AppOpen
+import dk.nodes.nstack.kotlin.models.Empty
 import dk.nodes.nstack.kotlin.models.Error
 import dk.nodes.nstack.kotlin.models.Result
 import dk.nodes.nstack.kotlin.provider.GsonProvider
@@ -13,6 +14,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
 import org.junit.Before
+import java.util.concurrent.CountDownLatch
 
 
 class NetworkManagerTestFake : TestCase() {
@@ -33,6 +35,18 @@ class NetworkManagerTestFake : TestCase() {
     fun shutdown() {
         mockWebServer.shutdown()
     }
+
+//    fun testNotSuspendFunctions() {
+//        val latch = CountDownLatch(1)
+//        manager.postAppOpen(AppOpenSettingsMockFactory.getAppSettings(), "", {
+//
+//            latch.countDown()
+//        }, {
+//
+//            latch.countDown()
+//        })
+//        latch.await()
+//    }
 
     fun testLoadTranslation() {
         mockWebServer.setResponse("response.json", 200)
@@ -81,12 +95,31 @@ class NetworkManagerTestFake : TestCase() {
         assert(response == unknowError)
     }
 
+    fun testPostMessageSeenSuccess() {
+        mockWebServer.setResponse("response.json", 200)
+        val response = runBlocking {
+            manager.postMessageSeen(AppOpenSettingsMockFactory.getAppSettings(), 1)
+        }
+        val successResult = Result.Success(value = Empty)
+        assert(response == successResult)
+    }
 
-    fun testTestPostAppOpen() {}
+    fun testPostMessageSeenNetworkError() {
+        val response = runBlocking {
+            manager.postMessageSeen(AppOpenSettingsMockFactory.getAppSettings(), 1)
+        }
+        val networkError = Result.Error(Error.NetworkError)
+        assert(response == networkError)
+    }
 
-    fun testPostMessageSeen() {}
-
-    fun testTestPostMessageSeen() {}
+    fun testPostMessageSeenApiError() {
+        mockWebServer.setResponse("response.json", 500)
+        val response = runBlocking {
+            manager.postMessageSeen(AppOpenSettingsMockFactory.getAppSettings(), 1)
+        }
+        val apiError = Result.Error(Error.ApiError(errorCode = 500))
+        assert(response == apiError)
+    }
 
     fun testPostRateReminderSeen() {}
 
